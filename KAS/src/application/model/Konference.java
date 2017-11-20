673d87c84656b875680e1814b46658a0387ad328
Anders <Anders@10.24.3.18>
2017-11-20 09:45:49 +0100

    ehm wut

--- KAS/src/application/model/Konference.java
+++ KAS/src/application/model/Konference.java
@@ -1,5 +1,17 @@
 package application.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Konference {
+    private String navn;
+    private ArrayList<Tilmelding> tilmeldinger;
+    private Date start;
+    private Date slut;
+    private String adresse;
+
+    public Konference(String navn, ArrayList<Tilmelding> tilmeldinger, Date start, Date slut, String adresse) {
+
+    }
 
 }
