29c8031a1de6a471fe1dd563212e2a789ceb7f9c
capper <capper@10.24.2.144>
2017-11-20 10:40:53 +0100

    Done

--- KAS/src/application/model/Tilmelding.java
+++ KAS/src/application/model/Tilmelding.java
@@ -1,5 +1,26 @@
 package application.model;
 
-public class Tilmelding {
+import java.util.Date;
 
+public class Tilmelding {
+    private boolean erFordragsholder;
+    private Date ankomstdato;
+    private Date afrejsedato;
+    private String ledsager;
+    private Hotel hotel;
+    private Udflugt udflugt;
+    private Deltager deltager;
+    private double samletPris;
+
+    public Tilmelding(boolean erFordragsholder, Date ankomstdato, Date afrejsedato, String ledsager, Hotel hotel,
+            Udflugt udflugt, Deltager deltager, double samletPris) {
+        this.erFordragsholder = erFordragsholder;
+        this.ankomstdato = ankomstdato;
+        this.afrejsedato = afrejsedato;
+        this.ledsager = ledsager;
+        this.hotel = hotel;
+        this.udflugt = udflugt;
+        this.deltager = deltager;
+        this.samletPris = samletPris;
+    }
 }
