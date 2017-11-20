8f6823245b90e926120768112a9faa536bd4832f
capper <capper@10.24.2.144>
2017-11-20 10:29:34 +0100

    asd

--- KAS/src/application/model/Udflugt.java
+++ KAS/src/application/model/Udflugt.java
@@ -8,7 +8,7 @@ public class Udflugt {
     private double pris;
     private boolean frokost;
     private String beskrivelse;
-    private String navns;
+    private String navn;
 
     public Udflugt(String sted, Date dato, double pris, boolean frokost, String beskrivelse, String navn) {
         this.sted = sted;
