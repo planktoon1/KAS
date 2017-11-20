5ab6ca53e3802b5323004efd0cfbd1084ca59e26
capper <capper@10.24.2.144>
2017-11-20 10:22:13 +0100

    Jeg har ændret

--- KAS/src/application/model/Udflugt.java
+++ KAS/src/application/model/Udflugt.java
@@ -1,5 +1,22 @@
 package application.model;
 
-public class Udflugt {
+import java.util.Date;
 
+public class Udflugt {
+    private String sted;
+    private Date dato;
+    private double pris;
+    private boolean frokost;
+    private String beskrivelse;
+    private String navn;
+
+    public Udflugt(String sted, Date dato, double pris, boolean frokost, String beskrivelse, String navn) {
+        this.sted = sted;
+        this.dato = dato;
+        this.pris = pris;
+        this.frokost = frokost;
+        this.beskrivelse = beskrivelse;
+        this.navn = navn;
+
+    }
 }
