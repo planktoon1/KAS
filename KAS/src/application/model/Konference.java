f629c0088111cb1435d909cb656be84de4a3424c
Anders <Anders@10.24.3.18>
2017-11-20 11:39:35 +0100

    ll,,l

--- KAS/src/application/model/Konference.java
+++ KAS/src/application/model/Konference.java
@@ -1,13 +1,14 @@
 package application.model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Konference {
     private String navn;
     private ArrayList<Tilmelding> tilmeldinger;
-    private Date start;
-    private Date slut;
+    private LocalDate start;
+    private LocalDate slut;
     private String adresse;
 
     public Konference(String navn, Date start, Date slut, String adresse) {
