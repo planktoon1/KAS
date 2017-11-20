ce6d2676106fdf0edad3ff7d1b2d76c38a8af7c3
Anders <Anders@10.24.3.18>
2017-11-20 11:51:41 +0100

    kplæl

--- KAS/src/gui/KonferenceDialog.java
+++ KAS/src/gui/KonferenceDialog.java
@@ -2,8 +2,7 @@ package gui;
 
 import java.time.LocalDate;
 
-import application.model.Company;
-import application.service.Service;
+import application.model.Konference;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -16,139 +15,139 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class KonferenceDialog extends Stage {
-	private final Controller controller = new Controller();
+    private final Controller controller = new Controller();
 
-	/** Note: company is nullable. */
-	public KonferenceDialog(String title, Company company) {
-		controller.company = company;
+    /** Note: company is nullable. */
+    public KonferenceDialog(String title, Konference konference) {
+        controller.konference = konference;
 
-		this.initModality(Modality.APPLICATION_MODAL);
-		this.setResizable(false);
+        this.initModality(Modality.APPLICATION_MODAL);
+        this.setResizable(false);
 
-		this.setTitle(title);
-		GridPane pane = new GridPane();
-		this.initContent(pane);
+        this.setTitle(title);
+        GridPane pane = new GridPane();
+        this.initContent(pane);
 
-		Scene scene = new Scene(pane);
-		this.setScene(scene);
-	}
+        Scene scene = new Scene(pane);
+        this.setScene(scene);
+    }
 
-	// -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
 
-	private final TextField txfName = new TextField(), txfAdresse = new TextField();
-	private final Label lblError = new Label();
-	private final DatePicker startDatePicker = new DatePicker(), slutDatePicker = new DatePicker();
+    private final TextField txfName = new TextField(), txfAdresse = new TextField();
+    private final Label lblError = new Label();
+    private final DatePicker startDatePicker = new DatePicker(), slutDatePicker = new DatePicker();
 
-	private void initContent(GridPane pane) {
-		pane.setPadding(new Insets(20, 20, 0, 20));
-		pane.setHgap(10);
-		pane.setVgap(5);
-		pane.setGridLinesVisible(false);
+    private void initContent(GridPane pane) {
+        pane.setPadding(new Insets(20, 20, 0, 20));
+        pane.setHgap(10);
+        pane.setVgap(5);
+        pane.setGridLinesVisible(false);
 
-		Label lblNavn = new Label("Konference navn");
-		pane.add(lblNavn, 0, 0);
+        Label lblNavn = new Label("Konference navn");
+        pane.add(lblNavn, 0, 0);
 
-		pane.add(txfName, 0, 1);
-		txfName.setPrefWidth(200);
+        pane.add(txfName, 0, 1);
+        txfName.setPrefWidth(200);
 
-		Label lblAdresse = new Label("Adresse (Post nr., By, Vej, Vej nr.)");
-		pane.add(lblAdresse, 0, 2);
-		GridPane.setMargin(lblAdresse, new Insets(10, 0, 0, 0));
+        Label lblAdresse = new Label("Adresse (Post nr., By, Vej, Vej nr.)");
+        pane.add(lblAdresse, 0, 2);
+        GridPane.setMargin(lblAdresse, new Insets(10, 0, 0, 0));
 
-		pane.add(txfAdresse, 0, 3);
+        pane.add(txfAdresse, 0, 3);
 
-		Label lblStartDato = new Label("Start Dato");
-		pane.add(lblStartDato, 0, 4);
-		GridPane.setMargin(lblStartDato, new Insets(10, 0, 0, 0));
-		startDatePicker.setShowWeekNumbers(true);
-		pane.add(startDatePicker, 0, 5);
+        Label lblStartDato = new Label("Start Dato");
+        pane.add(lblStartDato, 0, 4);
+        GridPane.setMargin(lblStartDato, new Insets(10, 0, 0, 0));
+        startDatePicker.setShowWeekNumbers(true);
+        pane.add(startDatePicker, 0, 5);
 
-		Label lblSlutDato = new Label("Slut Dato");
-		pane.add(lblSlutDato, 0, 6);
-		GridPane.setMargin(lblSlutDato, new Insets(10, 0, 0, 0));
-		slutDatePicker.setShowWeekNumbers(true);
-		pane.add(slutDatePicker, 0, 7);
+        Label lblSlutDato = new Label("Slut Dato");
+        pane.add(lblSlutDato, 0, 6);
+        GridPane.setMargin(lblSlutDato, new Insets(10, 0, 0, 0));
+        slutDatePicker.setShowWeekNumbers(true);
+        pane.add(slutDatePicker, 0, 7);
 
-		Button btnCancel = new Button("Annuller");
-		pane.add(btnCancel, 0, 8);
-		GridPane.setHalignment(btnCancel, HPos.LEFT);
-		GridPane.setMargin(btnCancel, new Insets(10, 0, 0, 30));
-		btnCancel.setOnAction(event -> controller.cancelAction());
+        Button btnCancel = new Button("Annuller");
+        pane.add(btnCancel, 0, 8);
+        GridPane.setHalignment(btnCancel, HPos.LEFT);
+        GridPane.setMargin(btnCancel, new Insets(10, 0, 0, 30));
+        btnCancel.setOnAction(event -> controller.cancelAction());
 
-		Button btnOK = new Button("OK");
-		pane.add(btnOK, 0, 8);
-		GridPane.setHalignment(btnOK, HPos.RIGHT);
-		GridPane.setMargin(btnOK, new Insets(10, 30, 0, 0));
-		btnOK.setOnAction(event -> controller.okAction());
+        Button btnOK = new Button("OK");
+        pane.add(btnOK, 0, 8);
+        GridPane.setHalignment(btnOK, HPos.RIGHT);
+        GridPane.setMargin(btnOK, new Insets(10, 30, 0, 0));
+        btnOK.setOnAction(event -> controller.okAction());
 
-		pane.add(lblError, 0, 9);
-		GridPane.setMargin(lblError, new Insets(0, 0, 10, 0));
-		lblError.setStyle("-fx-text-fill: red");
+        pane.add(lblError, 0, 9);
+        GridPane.setMargin(lblError, new Insets(0, 0, 10, 0));
+        lblError.setStyle("-fx-text-fill: red");
 
-		controller.updateControls();
-	}
+        controller.updateControls();
+    }
 
-	/**
-	 * Return true, if a company is created or updated.
-	 */
-	public boolean getResult() {
-		return controller.result;
-	}
+    /**
+     * Return true, if a company is created or updated.
+     */
+    public boolean getResult() {
+        return controller.result;
+    }
 
-	// -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
 
-	private class Controller {
-		private Company company;
-		private boolean result = false;
+    private class Controller {
+        private Konference konference;
+        private boolean result = false;
 
-		public void updateControls() {
-			if (company != null) {
-				txfName.setText(company.getName());
-				txfAdresse.setText("" + company.getHours());
-			} else {
-				txfName.clear();
-				txfAdresse.clear();
-			}
-			lblError.setText("");
-		}
+        public void updateControls() {
+            if (konference != null) {
+                txfName.setText(konference.getNavn());
+                txfAdresse.setText("" + konference.getAdresse());
+            } else {
+                txfName.clear();
+                txfAdresse.clear();
+            }
+            lblError.setText("");
+        }
 
-		// Cancel button action
-		public void cancelAction() {
-			result = false;
-			KonferenceDialog.this.hide();
-		}
+        // Cancel button action
+        public void cancelAction() {
+            result = false;
+            KonferenceDialog.this.hide();
+        }
 
-		// OK button action
-		public void okAction() {
-			LocalDate startDate = startDatePicker.getValue();
-			LocalDate slutDate = slutDatePicker.getValue();
-			String name = txfName.getText().trim();
-			String adresse = txfAdresse.getText().trim();
+        // OK button action
+        public void okAction() {
+            LocalDate startDate = startDatePicker.getValue();
+            LocalDate slutDate = slutDatePicker.getValue();
+            String name = txfName.getText().trim();
+            String adresse = txfAdresse.getText().trim();
 
-			if (name.length() == 0) {
-				lblError.setText("Name is empty");
-				return;
-			}
+            if (name.length() == 0) {
+                lblError.setText("Name is empty");
+                return;
+            }
 
-			int hours = -1;
-			try {
-				hours = Integer.parseInt(txfAdresse.getText().trim());
-			} catch (NumberFormatException ex) {
-				// do nothing
-			}
-			if (hours < 0) {
-				lblError.setText("Hours is not a positive number");
-				return;
-			}
+            int hours = -1;
+            try {
+                hours = Integer.parseInt(txfAdresse.getText().trim());
+            } catch (NumberFormatException ex) {
+                // do nothing
+            }
+            if (hours < 0) {
+                lblError.setText("Hours is not a positive number");
+                return;
+            }
 
-			// Call Service methods
-			if (company != null)
-				Service.updateCompany(company, name, hours);
-			else
-				Service.createCompany(name, hours);
+            // Call Service methods
+//			if (konference != null)
+//				Service.updateCompany(konference, name, hours);
+//			else
+//				Service.createCompany(name, hours);
 
-			result = true;
-			KonferenceDialog.this.hide();
-		}
-	}
+            result = true;
+            KonferenceDialog.this.hide();
+        }
+    }
 }
--- KAS/src/gui/KonferencePane.java
+++ KAS/src/gui/KonferencePane.java
@@ -1,7 +1,6 @@
 package gui;
 
-import application.model.Company;
-import application.model.Employee;
+import application.model.Konference;
 import application.service.Service;
 import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
@@ -17,157 +16,153 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class KonferencePane extends GridPane {
-	private final Controller controller = new Controller();
+    private final Controller controller = new Controller();
 
-	public KonferencePane() {
-		this.initContent();
-	}
+    public KonferencePane() {
+        this.initContent();
+    }
 
-	// -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
 
-	private final TextField txfNavn = new TextField(), txfHours = new TextField();
-	private final TextArea txaEmps = new TextArea();
-	private final ListView<Company> lvwKonferencer = new ListView<>();
+    private final TextField txfNavn = new TextField(), txfHours = new TextField();
+    private final TextArea txaEmps = new TextArea();
+    private final ListView<Konference> lvwKonferencer = new ListView<>();
 
-	private void initContent() {
-		this.setPadding(new Insets(20));
-		this.setHgap(20);
-		this.setVgap(10);
-		this.setGridLinesVisible(false);
+    private void initContent() {
+        this.setPadding(new Insets(20));
+        this.setHgap(20);
+        this.setVgap(10);
+        this.setGridLinesVisible(false);
 
-		Label lblKonf = new Label("Konferencer");
-		this.add(lblKonf, 0, 0);
+        Label lblKonf = new Label("Konferencer");
+        this.add(lblKonf, 0, 0);
 
-		this.add(lvwKonferencer, 0, 1, 1, 3);
-		lvwKonferencer.setPrefWidth(200);
-		lvwKonferencer.setPrefHeight(200);
+        this.add(lvwKonferencer, 0, 1, 1, 3);
+        lvwKonferencer.setPrefWidth(200);
+        lvwKonferencer.setPrefHeight(200);
 
-		ChangeListener<Company> listener = (ov, o, n) -> controller.selectedKonferenceChanged();
-		lvwKonferencer.getSelectionModel().selectedItemProperty().addListener(listener);
+        ChangeListener<Konference> listener = (ov, o, n) -> controller.selectedKonferenceChanged();
+        lvwKonferencer.getSelectionModel().selectedItemProperty().addListener(listener);
 
-		Label lblNavn = new Label("Navn:");
-		this.add(lblNavn, 1, 1);
+        Label lblNavn = new Label("Navn:");
+        this.add(lblNavn, 1, 1);
 
-		this.add(txfNavn, 2, 1);
-		txfNavn.setEditable(false);
+        this.add(txfNavn, 2, 1);
+        txfNavn.setEditable(false);
 
-		Label lblHours = new Label("Weekly Hours:");
-		this.add(lblHours, 1, 2);
+        Label lblHours = new Label("Weekly Hours:");
+        this.add(lblHours, 1, 2);
 
-		this.add(txfHours, 2, 2);
-		txfHours.setEditable(false);
+        this.add(txfHours, 2, 2);
+        txfHours.setEditable(false);
 
-		Label lblEmps = new Label("Employees:");
-		this.add(lblEmps, 1, 3);
-		GridPane.setValignment(lblEmps, VPos.BASELINE);
-		lblEmps.setPadding(new Insets(4, 0, 4, 0));
+        Label lblEmps = new Label("Employees:");
+        this.add(lblEmps, 1, 3);
+        GridPane.setValignment(lblEmps, VPos.BASELINE);
+        lblEmps.setPadding(new Insets(4, 0, 4, 0));
 
-		this.add(txaEmps, 2, 3);
-		txaEmps.setPrefWidth(200);
-		txaEmps.setPrefHeight(100);
-		txaEmps.setEditable(false);
+        this.add(txaEmps, 2, 3);
+        txaEmps.setPrefWidth(200);
+        txaEmps.setPrefHeight(100);
+        txaEmps.setEditable(false);
 
-		HBox hbxButtons = new HBox(40);
-		this.add(hbxButtons, 0, 4, 3, 1);
-		hbxButtons.setPadding(new Insets(10, 0, 0, 0));
-		hbxButtons.setAlignment(Pos.BASELINE_CENTER);
+        HBox hbxButtons = new HBox(40);
+        this.add(hbxButtons, 0, 4, 3, 1);
+        hbxButtons.setPadding(new Insets(10, 0, 0, 0));
+        hbxButtons.setAlignment(Pos.BASELINE_CENTER);
 
-		Button btnAdd = new Button("Tilføj Konference");
-		hbxButtons.getChildren().add(btnAdd);
-		btnAdd.setOnAction(event -> controller.createAction());
+        Button btnAdd = new Button("Tilføj Konference");
+        hbxButtons.getChildren().add(btnAdd);
+        btnAdd.setOnAction(event -> controller.createAction());
 
-		Button btnRediger = new Button("Rediger");
-		hbxButtons.getChildren().add(btnRediger);
-		btnRediger.setOnAction(event -> controller.redigerAction());
+        Button btnRediger = new Button("Rediger");
+        hbxButtons.getChildren().add(btnRediger);
+        btnRediger.setOnAction(event -> controller.redigerAction());
 
-		controller.fillLvwCompanies();
-	}
+        controller.fillLvwCompanies();
+    }
 
-	// -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
 
-	/**
-	 * Updates the content of controls in this pane.
-	 */
-	public void updateControls() {
-		controller.updateControls();
-	}
+    /**
+     * Updates the content of controls in this pane.
+     */
+    public void updateControls() {
+        controller.updateControls();
+    }
 
-	// -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
 
-	private class Controller {
-		private KonferenceDialog createDialog, updateDialog;
+    private class Controller {
+        private KonferenceDialog createDialog, updateDialog;
 
-		public void fillLvwCompanies() {
-			lvwKonferencer.getItems().setAll(Service.getAllCompanies());
-			if (lvwKonferencer.getItems().size() > 0)
-				lvwKonferencer.getSelectionModel().select(0);
-		}
+        public void fillLvwCompanies() {
+            lvwKonferencer.getItems().setAll(Service.getAllkonferencer());
+            if (lvwKonferencer.getItems().size() > 0)
+                lvwKonferencer.getSelectionModel().select(0);
+        }
 
-		public void updateControls() {
-			Company company = lvwKonferencer.getSelectionModel().getSelectedItem();
-			if (company != null) {
-				txfNavn.setText(company.getName());
-				txfHours.setText("" + company.getHours());
-				StringBuilder sb = new StringBuilder();
-				for (Employee emp : company.getEmployees()) {
-					sb.append(emp + "\n");
-				}
-				txaEmps.setText(sb.toString());
-			} else {
-				txfNavn.clear();
-				txfHours.clear();
-				txaEmps.clear();
-			}
-		}
+        public void updateControls() {
+            Konference konference = lvwKonferencer.getSelectionModel().getSelectedItem();
+            if (konference != null) {
+                txfNavn.setText(konference.getNavn());
+                txfHours.setText("" + konference.getAdresse());
 
-		// --------------------------------------------------------------------
+            } else {
+                txfNavn.clear();
+                txfHours.clear();
+                txaEmps.clear();
+            }
+        }
 
-		// Create button action
-		public void createAction() {
-			if (createDialog == null) {
-				createDialog = new KonferenceDialog("Create Company", null);
-				Stage stage = (Stage) lvwKonferencer.getScene().getWindow();
-				createDialog.initOwner(stage);
-			}
+        // --------------------------------------------------------------------
 
-			createDialog.showAndWait();
-			// ... wait for the dialog to close
+        // Create button action
+        public void createAction() {
+            if (createDialog == null) {
+                createDialog = new KonferenceDialog("Create Konference", null);
+                Stage stage = (Stage) lvwKonferencer.getScene().getWindow();
+                createDialog.initOwner(stage);
+            }
 
-			boolean isCreated = createDialog.getResult();
-			if (isCreated) {
-				lvwKonferencer.getItems().setAll(Service.getAllCompanies());
-				int index = lvwKonferencer.getItems().size() - 1;
-				lvwKonferencer.getSelectionModel().select(index);
-			}
-		}
+            createDialog.showAndWait();
+            // ... wait for the dialog to close
 
-		// Update button action
-		public void redigerAction() {
-			// Company company = lvwKonferencer.getSelectionModel().getSelectedItem();
-			// if (company == null)
-			// return;
-			//
-			// if (updateDialog == null) {
-			// updateDialog = new CompanyDialog("Update Company", company);
-			// Stage stage = (Stage) lvwKonferencer.getScene().getWindow();
-			// updateDialog.initOwner(stage);
-			// }
-			//
-			// updateDialog.showAndWait();
-			// // ... wait for the dialog to close
-			//
-			// boolean isUpdated = updateDialog.getResult();
-			// if (isUpdated) {
-			// int selectIndex = lvwKonferencer.getSelectionModel().getSelectedIndex();
-			// lvwKonferencer.getItems().setAll(Service.getAllCompanies());
-			// lvwKonferencer.getSelectionModel().select(selectIndex);
-			// }
-		}
+            boolean isCreated = createDialog.getResult();
+            if (isCreated) {
+                lvwKonferencer.getItems().setAll(Service.getAllkonferencer());
+                int index = lvwKonferencer.getItems().size() - 1;
+                lvwKonferencer.getSelectionModel().select(index);
+            }
+        }
 
-		// Selected item in lvwCompanies changed
-		public void selectedKonferenceChanged() {
-			this.updateControls();
-		}
-	}
+        // Update button action
+        public void redigerAction() {
+            // Company company = lvwKonferencer.getSelectionModel().getSelectedItem();
+            // if (company == null)
+            // return;
+            //
+            // if (updateDialog == null) {
+            // updateDialog = new CompanyDialog("Update Company", company);
+            // Stage stage = (Stage) lvwKonferencer.getScene().getWindow();
+            // updateDialog.initOwner(stage);
+            // }
+            //
+            // updateDialog.showAndWait();
+            // // ... wait for the dialog to close
+            //
+            // boolean isUpdated = updateDialog.getResult();
+            // if (isUpdated) {
+            // int selectIndex = lvwKonferencer.getSelectionModel().getSelectedIndex();
+            // lvwKonferencer.getItems().setAll(Service.getAllCompanies());
+            // lvwKonferencer.getSelectionModel().select(selectIndex);
+            // }
+        }
+
+        // Selected item in lvwCompanies changed
+        public void selectedKonferenceChanged() {
+            this.updateControls();
+        }
+    }
 
 }
