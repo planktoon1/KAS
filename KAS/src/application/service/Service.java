e2528dbaa44caca5a3ca5fdb57a1aa1f3f373f93
Anders <Anders@10.24.3.18>
2017-11-20 11:04:44 +0100

    ny

--- KAS/src/application/service/Service.java
+++ KAS/src/application/service/Service.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import application.model.Konference;
-import application.model.Tilmelding;
 import storage.Storage;
 
 public class Service {
@@ -19,9 +18,9 @@ public class Service {
     /**
      * Creater en ny konference.<br />
      */
-    public static Konference createKonference(String navn, ArrayList<Tilmelding> tilmeldinger, Date start, Date slut,
+    public static Konference createKonference(String navn, Date start, Date slut,
             String adresse) {
-        Konference konference = new Konference(navn, tilmeldinger, start, slut, adresse);
+        Konference konference = new Konference(navn, start, slut, adresse);
         Storage.storeKonference(konference);
         return konference;
     }
