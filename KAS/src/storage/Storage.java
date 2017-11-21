b7d57dba5f74cce274a0e9f53617225c51349783
Anders <anders.raunstrup@hotmail.com>
2017-11-21 13:22:46 +0100

    kkkk

--- KAS/src/storage/Storage.java
+++ KAS/src/storage/Storage.java
@@ -1,10 +1,12 @@
 package storage;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import application.model.Deltager;
 import application.model.Firma;
 import application.model.Hotel;
+import application.model.HotelTillæg;
 import application.model.Konference;
 import application.model.Tilmelding;
 import application.model.Udflugt;
@@ -44,7 +46,38 @@ public class Storage {
     }
 
     public static void initStorage() {
-    	
+        LocalDate d1 = LocalDate.now();
+        LocalDate d2 = d1.plusDays(3);
+
+        LocalDate d3 = LocalDate.now().plusMonths(3);
+        LocalDate d4 = d3.plusDays(3);
+
+        Konference k1 = new Konference("konference 1: Star Trekz", d1, d2, "Sønderhøj 30");
+        Konference k2 = new Konference("konference 2: Star warz", d3, d4, "Sønderhøj 30");
+
+        storeKonference(k1);
+        storeKonference(k2);
+
+        HotelTillæg ht1 = new HotelTillæg("WIFI", 300.0);
+        HotelTillæg ht2 = new HotelTillæg("Bad", 150.0);
+        HotelTillæg ht3 = new HotelTillæg("Håndklæder", 50.0);
+
+        ArrayList<HotelTillæg> hotelTillæg = new ArrayList<HotelTillæg>();
+        hotelTillæg.add(ht1);
+        hotelTillæg.add(ht2);
+        hotelTillæg.add(ht3);
+
+        Hotel h1 = new Hotel("nigga naps", "bullshitstræde 69", 800.0, hotelTillæg);
+        Hotel h2 = new Hotel("sleepz", "Viby torv", 1600.0, hotelTillæg);
+
+        storeHotel(h1);
+        storeHotel(h2);
+
+        Udflugt u1 = new Udflugt("Den mørke skov", d1.plusDays(1), 400.0, false,
+                "Rollespil i den mørke skov og du må ikke være med før du får lov", "Rollespil",
+                k1);
+
+        storeUdflugt(u1);
     }
 
 }
